fdc62f05c559eab9e9686b101f835fc43c413a8e
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-05 13:19:32 +0200

    made pageObjects and locators

---
+++ pageObjects/HeaderObject.py
@@ -0,0 +1,11 @@
+class HeaderObject:
+    LOGO = '//*[@class="header--flex-middle--2QeVR header--logo--rd7-H"]'
+    CATEGORIES = '//*[@class="popper-module--popper--2BpLn header--gap-button--1p5bX"]'
+    SEARCH_INPUT = '//*[@data-testid="search-input"]'
+    UDEMY_BUSINESS = '//*[@id="u78-popper-trigger--8"]'
+    TEACH_ON_UDEMY = '//*[@id="u78-popper-trigger--10"]'
+    BASKET = '//*[@id="udemy"]//div[4]/div[5]'
+    LOG_IN = '//*[@data-purpose="header-login"]'
+    SIGN_UP = '//*[@data-purpose="header-signup"]'
+    CHOOSE_A_LANGUAGE = '//*[@id="udemy"]//div[8]/button'
+
