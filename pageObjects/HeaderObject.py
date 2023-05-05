e97af26ca5ff5db8c7d1cc41e043fbda6b93ca2f
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-05 15:21:17 +0200

    added locator sub_categories

--- pageObjects/HeaderObject.py
+++ pageObjects/HeaderObject.py
@@ -1,6 +1,9 @@
 class HeaderObject:
     LOGO = '//*[@class="header--flex-middle--2QeVR header--logo--rd7-H"]'
+
     CATEGORIES = '//*[@class="popper-module--popper--2BpLn header--gap-button--1p5bX"]'
+    SUB_CATEGORIES = '//*[@class="js-browse-nav-level-one browse-nav--nav--1ssWv"]'
+
     SEARCH_INPUT = '//*[@data-testid="search-input"]'
     UDEMY_BUSINESS = '//*[@id="u78-popper-trigger--8"]'
     TEACH_ON_UDEMY = '//*[@id="u78-popper-trigger--10"]'
