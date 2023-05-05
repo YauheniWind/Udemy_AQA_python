7084376ebfe83f68557e223bff0a809a7cc0a041
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-05 14:50:08 +0200

    added new test

--- tests/tests.py
+++ tests/tests.py
@@ -5,5 +5,6 @@ def test_logo(browser, open_main_page):
     logo_test = HeaderPage(browser)
 
     logo_test.waiting_logo()
+    logo_test.find_element()
     logo_test.click_on_logo()
     logo_test.current_url()
