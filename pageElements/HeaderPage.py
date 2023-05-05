3729fa929016cf6cc54244d816991df6afcf5e11
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-05 15:22:23 +0200

    made new methods in class HeaderPage

--- pageElements/HeaderPage.py
+++ pageElements/HeaderPage.py
@@ -9,13 +9,43 @@ class HeaderPage(Helper):
         self.elements = HeaderObject()
 
     def waiting_logo(self):
+        """
+        Метод ожидает элемент
+        """
         self.waiting_element(self.elements.LOGO)
 
-    def find_element(self):
+    def find_element_logo(self):
+        """
+        Метод находит елемент
+        """
         assert self.get_locator_by_xpath(self.elements.LOGO)
 
     def click_on_logo(self):
+        """
+        Метод кликает на элемент
+        """
         self.click_button(self.elements.LOGO)
 
     def url(self):
+        """
+        Метод сравнивает url адресс
+        """
         assert self.current_url() == 'https://www.udemy.com/?persist_locale=&locale=en_US'
+
+    def waiting_categories(self):
+        """
+        Метод ожидает элемент
+        """
+        self.waiting_element(self.elements.CATEGORIES)
+
+    def mouse_moving_to_categories(self):
+        """
+        Метод для передвежения мыши
+        """
+        self.mouse_moving(HeaderObject.CATEGORIES)
+
+    def find_element_sub_categories(self):
+        """
+        Метод ожидания сабкатегорий
+        """
+        assert self.get_locator_by_xpath(HeaderObject.SUB_CATEGORIES)
