0e755889aca4f7cd6ec4d0d994d8f1f2b5d3b9ee
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-05 13:20:09 +0200

    made pageElements and class Header

---
+++ pageElements/Header.py
@@ -0,0 +1,21 @@
+from Helper import Helper
+from pageObjects.HeaderObject import HeaderObject
+
+
+class Header(Helper):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+        self.elements = HeaderObject()
+
+    def waiting_logo(self):
+        self.waiting_element(self.elements.LOGO)
+
+    def find_element(self):
+        assert self.get_locator_by_xpath(self.elements.LOGO)
+
+    def click_on_logo(self):
+        self.click_button(self.elements.LOGO)
+
+    def url(self):
+        assert self.current_url() == 'https://www.udemy.com/?persist_locale=&locale=en_US'
