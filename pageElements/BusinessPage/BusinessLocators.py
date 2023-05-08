314e61eb9faa78207acace923dceb59968c35008
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-08 14:06:07 +0200

    made Businesspage

---
+++ pageElements/BusinessPage/BusinessLocators.py
@@ -0,0 +1,14 @@
+class BusinessLocators:
+    MODAL_COOKIE_WINDOW = '//*[@class="ot-sdk-container"]'
+    ACCEPT_COOKIE = '//*[@id="onetrust-accept-btn-handler"]'
+    ACCEPT_PRIVACY = '//*[@id="accept-recommended-btn-handler"]'
+
+    BUSINESS_LOGO = '//*[@class="custom-logo-link"]'
+
+    WORK_EMAIL_INPUT = '//*[@id="Email"]'
+    FIRST_NAME_INPUT = '//*[@id="FirstName"]'
+    LAST_NAME_INPUT = '//*[@id="LastName"]'
+    PHONE_NUMBER_INPUT = '//*[@id="Phone"]'
+    COMPANY_NAME_INPUT = '//*[@id="Company"]'
+    TITLE_INPUT = '//*[@id="Title"]'
+    COUNTRY_SELECT = '//*[@id="Person_Country__c"]'
---
+++ pageElements/BusinessPage/BusinessPage.py
@@ -0,0 +1,15 @@
+from Helper import Helper
+from pageElements.BusinessPage.BusinessLocators import BusinessLocators
+
+
+class BusinessPage(Helper):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+        self.elements = BusinessLocators()
+
+    def assert_logo_is_displayed(self):
+        assert self.get_locator_by_xpath(self.elements.BUSINESS_LOGO)
+
+    def click_on_logo(self):
+        self.click_button(self.elements.BUSINESS_LOGO)
