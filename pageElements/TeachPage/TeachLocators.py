46141b9543135d2e9d0205642ea43b5bcf6c58f4
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-05-08 14:06:44 +0200

    made TeachPage

---
+++ pageElements/TeachPage/TeachLocators.py
@@ -0,0 +1,4 @@
+class TeachLocators:
+    MAIN_BANNER = '//*[@class="hero-header--billboard-container--sYvLn"]'
+    GET_START_BUTTON = '//*[@data-purpose="bai-cta"]'
+    BECOME_INSTRUCTOR = '//*[@data-module-id="auth"]'
---
+++ pageElements/TeachPage/TeachPage.py
@@ -0,0 +1,21 @@
+from Helper import Helper
+from pageElements.TeachPage.TeachLocators import TeachLocators
+
+
+class TeachPage(Helper):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+        self.elements = TeachLocators()
+
+    def delete_all_cookie(self):
+        self.delete_all_cookies()
+
+    def assert_find_main_banner(self):
+        assert self.get_locator_by_xpath(self.elements.MAIN_BANNER)
+
+    def click_get_started(self):
+        self.click_button(self.elements.GET_START_BUTTON)
+
+    def assert_modal_window_is_displayed(self):
+        assert self.get_locator_by_xpath(self.elements.BECOME_INSTRUCTOR)
