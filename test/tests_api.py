9f29db986370c281c83adf0da2509e7aeda599a1
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2023-06-16 11:55:26 +0200

    rename tests

--- test/tests_api.py
+++ test/tests_api.py
@@ -1,31 +1,31 @@
 import allure
 
-from HttpRequestsService.http_requests_service import HttpRequestsService
+from serviceObject.http_service import HttpService
 
 
 @allure.suite("Test API")
 class TestAPI:
     @allure.title("Test API: get success status")
     def test_get_request(self):
-        get_request = HttpRequestsService()
-        get_request.test_get_status_200()
+        get_request = HttpService()
+        get_request.get_status_200()
 
     @allure.title("Test API: create task")
     def test_create_task(self):
-        create_tasks = HttpRequestsService()
-        create_tasks.test_create_task()
+        create_task = HttpService()
+        create_task.create_task()
 
     @allure.title("Test API: update task")
     def test_update_task(self):
-        update_task = HttpRequestsService()
-        update_task.test_update_data()
+        update_task = HttpService()
+        update_task.update_data()
 
     @allure.title("Test API: create tasks")
     def test_list_tasks(self):
-        list_tasks = HttpRequestsService()
-        list_tasks.test_get_list_tasks()
+        list_tasks = HttpService()
+        list_tasks.get_list_tasks()
 
     @allure.title("Test API: delete task")
     def test_delete_task(self):
-        delete_task = HttpRequestsService()
-        delete_task.test_delete_task()
+        delete_task = HttpService()
+        delete_task.delete_task()
